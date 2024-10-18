b996d068e52a3dfd2e1a4c0e270f1c7dd518157a
krishnaYadav001 <152070584+krishnaYadav001@users.noreply.github.com>
2024-10-18 20:17:55 +0530

    Update config.ts

--- src/lib/appwrite/config.ts
+++ src/lib/appwrite/config.ts
@@ -1,13 +1,13 @@
 import { Client, Account, Databases, Storage, Avatars } from "appwrite";
 
 export const appwriteConfig = {
-  url: 'https://cloud.appwrite.io/v1',
-  projectId: '65632f6861ff5a074b3e',
-  databaseId: '65661bec00373d7c1274',
-  storageId: '65661b90d8e205e0427b',
-  userCollectionId: '65661cc81cb141f88166',
-  postCollectionId: '65661c696720e28a4d33',
-  savesCollectionId: '65661d827cd5ec0e788a',
+  url: '',
+  projectId: '',
+  databaseId: '',
+  storageId: '',
+  userCollectionId: '',
+  postCollectionId: '',
+  savesCollectionId: '',
 };
 
 export const client = new Client();
